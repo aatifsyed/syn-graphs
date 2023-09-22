1e07b53ab641da47d80d10719072aaab4161c285
Aatif Syed <aatifsyedyp@gmail.com>
2023-09-22 05:33:02 +0100

    refactor: add new test vector sources

--- refresh-test-vectors.bash
+++ refresh-test-vectors.bash
@@ -1,21 +1,33 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-root=$PWD/test-vectors
-rm -rf "$root"
-mkdir -- "$root"
+temp=$PWD/target/test-vector-sources
+rm -rf "$temp"
+mkdir -- "$temp"
 
+test_vectors_root=$PWD/test-vectors
+test_vectors=$test_vectors_root/dot
 
-graphviz_gallery=$root/graphviz-gallery
-mkdir -- "$graphviz_gallery"
+rm -rf "$test_vectors_root"
+mkdir --parents -- "$test_vectors"
 
-commit=$(git ls-remote https://gitlab.com/graphviz/graphviz.gitlab.io | rg HEAD | awk '{ print $1 }')
-test -n "$commit"
-tarball_url=https://gitlab.com/graphviz/graphviz.gitlab.io/-/archive/$commit/graphviz.gitlab.io-main.tar.gz
+for repo in graphviz graphviz.gitlab.io; do
+    repo_url=https://gitlab.com/graphviz/$repo
+    commit=$(git ls-remote "$repo_url" | rg HEAD | awk '{ print $1 }')
+    test -n "$commit"
+    tarball_url=https://gitlab.com/graphviz/$repo/-/archive/$commit/$repo-$commit.tar.gz
 
-pushd -- "$graphviz_gallery"
-curl --silent --show-error --location "$tarball_url" \
-    | tar --extract --verbose --ungzip --wildcards '*/content/en/Gallery/*/*.gv.txt' --strip-components=5
-popd
+    # tar: --touch because some files may be from the future upstream
+    curl --silent --show-error --location "$tarball_url" \
+        | tar --extract --verbose --ungzip --touch --directory "$temp"
+    
+    echo "$commit" > "$test_vectors_root/$repo.commit"
+done
 
-echo "$commit" > "$root/graphviz-gallery.commit"
+for suffix in '*.gv' '*.gv.txt'; do
+    # sort for determinism
+    find "$temp" -name "$suffix" -and -type f -print0  \
+        | sort --zero-terminated \
+        | xargs --null cp --backup=numbered --verbose --target-directory="$test_vectors"
+done
+
