e85748086a674b0950d203f73aa519e99945012e
Aatif Syed <aatifsyedyp@gmail.com>
2023-09-22 05:37:13 +0100

    test: many more tests

--- tests/dot-vectors.rs
+++ tests/dot-vectors.rs
@@ -1,53 +1,47 @@
-use std::{
-    fmt::{self, Display},
-    str::FromStr as _,
-};
+use std::{error::Error, ffi::OsStr, fmt::Display, str::FromStr as _};
 
 use colored::Colorize as _;
 use either::Either;
 use include_dir::{include_dir, Dir};
 use miette::{Diagnostic, NamedSource, SourceOffset, SourceSpan};
 use pretty_assertions::Comparison;
-use proc_macro2::{LexError, TokenStream};
+use proc_macro2::{Span, TokenStream};
 use syn_graphs::{dot::Graph, unparse};
 use thiserror::Error;
 
-const GRAPHVIZ_GALLERY: Dir<'_> = include_dir!("$CARGO_MANIFEST_DIR/test-vectors/graphviz-gallery");
-
-fn parse(source_name: impl Display, source: &str) -> Result<Graph, Either<LexError, ParseError>> {
-    let removed_comments = source
-        .lines()
-        .filter(|it| !it.starts_with('#'))
-        .collect::<Vec<_>>()
-        .as_slice()
-        .join("\n");
-    match TokenStream::from_str(removed_comments.as_str()) {
-        Ok(token_stream) => match syn::parse2(token_stream) {
-            Ok(graph) => Ok(graph),
-            Err(syn_error) => Err(Either::Right(ParseError::new(
-                syn_error,
-                source_name,
-                removed_comments,
-            ))),
-        },
-        Err(lex_error) => Err(Either::Left(lex_error)),
-    }
-}
+const GRAPHVIZ_GALLERY: Dir<'_> = include_dir!("$CARGO_MANIFEST_DIR/test-vectors/dot");
+const SKIP: &[&str] = &[
+    "base.gv",         // syntax error online too
+    "b545.gv",         // syntax error online too
+    "b993.gv",         // uses `diGraph` instead of `digraph`
+    "multi.gv",        // don't support multiple graphs in one document
+    "polypoly.gv.~1~", // BUG?(aatifsyed)
+    "polypoly.gv.~2~", // BUG?(aatifsyed)
+    "world.gv.~2~",    // BUG?(aatifsyed)
+];
 
 #[test]
-fn gallery() {
-    let mut fail = vec![];
+fn vectors() {
+    let mut fail = 0;
     let mut skipped = 0;
     let mut pass = 0;
     for gallery_item in GRAPHVIZ_GALLERY.files() {
         let path = gallery_item.path();
         print!("vector {}...", path.display());
-        match parse(
-            path.display(),
-            gallery_item
-                .contents_utf8()
-                .expect("test cases are all utf-8"),
-        ) {
+        let Some(source) = gallery_item.contents_utf8() else {
+            println!("{} (non-utf-8)", "skipped".yellow());
+            skipped += 1;
+            continue;
+        };
+        if path
+            .file_name()
+            .is_some_and(|it| SKIP.iter().any(|skip| OsStr::new(skip) == it))
+        {
+            println!("{} (listed)", "skipped".yellow());
+            skipped += 1;
+            continue;
+        }
+        match parse(path.display(), source) {
             Ok(mut first) => {
                 let source2 = unparse::dot(&first);
                 match parse(format!("{} (round trip)", path.display()), source2.as_str()) {
@@ -58,96 +52,115 @@ fn gallery() {
                             true => {
                                 println!("{}", "ok".green());
                                 pass += 1;
+                                continue;
                             }
                             false => {
                                 println!("{} (mismatch)", "FAIL".red());
-                                fail.push(TestCaseFailure::Mismatch(first, second))
+                                println!("{}", Comparison::new(&first, &second));
                             }
                         }
                     }
-                    Err(Either::Left(lex_error)) => {
-                        println!("{} (second lex)", "FAIL".red());
-                        fail.push(TestCaseFailure::SecondLex(lex_error))
-                    }
-                    Err(Either::Right(parse_error)) => {
+                    Err(e) => {
                         println!("{} (second parse)", "FAIL".red());
-                        fail.push(TestCaseFailure::SecondParse(parse_error))
+                        println!("{}", e.pretty());
                     }
                 }
             }
-            Err(Either::Left(_lex_error)) => {
-                println!("{} (lex error)", "skipped".yellow());
-                skipped += 1;
-            }
-            Err(Either::Right(parse_error)) => {
+            Err(e) => {
                 println!("{} (first parse)", "FAIL".red());
-                fail.push(TestCaseFailure::FirstParse(parse_error));
+                println!("{}", e.pretty());
             }
         }
+        fail += 1;
     }
 
-    match fail.len() {
+    match fail {
         0 => println!("{pass} vectors succeeded ({skipped} skipped)."),
         nonzero => {
-            for fail in fail {
-                println!("{}", fail);
-            }
             panic!("{nonzero} vectors failed ({pass} succeeded, {skipped} skipped).")
         }
     }
 }
 
-enum TestCaseFailure {
-    FirstParse(ParseError),
-    SecondParse(ParseError),
-    SecondLex(LexError),
-    Mismatch(Graph, Graph),
-}
-
-impl fmt::Display for TestCaseFailure {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            TestCaseFailure::FirstParse(it) | TestCaseFailure::SecondParse(it) => {
-                f.write_str(&it.pretty())
-            }
-            TestCaseFailure::SecondLex(it) => f.write_fmt(format_args!("{}", it)),
-            TestCaseFailure::Mismatch(left, right) => {
-                f.write_fmt(format_args!("{}", Comparison::new(left, right)))
-            }
-        }
+fn parse(
+    source_name: impl Display,
+    source: &str,
+) -> Result<Graph, SpannedError<Either<proc_macro2::LexError, syn::Error>>> {
+    let source = source
+        .replace("\\N", "N")
+        .replace("\\G", "G")
+        .replace("\\E", "E")
+        .replace("\\<", "<")
+        .replace("\\>", ">")
+        .replace("\\l", "l")
+        .replace('«', "<<")
+        .replace('»', ">>");
+    let source = source // remove comments
+        .lines()
+        .filter(|it| !it.starts_with('#'))
+        .collect::<Vec<_>>()
+        .as_slice()
+        .join("\n");
+    match TokenStream::from_str(source.as_str()) {
+        Ok(token_stream) => match syn::parse2(token_stream) {
+            Ok(graph) => Ok(graph),
+            Err(syn_error) => Err(SpannedError::new(
+                syn_error.span(),
+                Either::Right(syn_error),
+                source_name,
+                source,
+            )),
+        },
+        Err(lex_error) => Err(SpannedError::new(
+            lex_error.span(),
+            Either::Left(lex_error),
+            source_name,
+            source,
+        )),
     }
 }
 
 #[derive(Diagnostic, Debug, Error)]
-#[error("test case failed")]
-pub struct ParseError {
+#[error("{}", inner)]
+pub struct SpannedError<E: Error + 'static> {
     #[source_code]
     src: NamedSource,
     #[source]
-    syn: syn::Error,
+    inner: E,
     #[label]
     span: SourceSpan,
 }
-
-impl ParseError {
-    pub fn new(syn: syn::Error, source_name: impl Display, source_text: String) -> Self {
-        let start = syn.span().start();
-        let end = syn.span().end();
+impl<E: Error + 'static> SpannedError<E> {
+    pub fn new(span: Span, inner: E, source_name: impl Display, source_text: String) -> Self {
+        let start = span.start();
+        let end = span.end();
         Self {
             span: SourceSpan::new(
                 SourceOffset::from_location(&source_text, start.line, start.column),
                 SourceOffset::from_location(&source_text, end.line, end.column),
             ),
             src: NamedSource::new(source_name.to_string(), source_text),
-            syn,
+            inner,
         }
     }
+
     pub fn pretty(&self) -> String {
         let renderer = miette::GraphicalReportHandler::new();
         let mut out = String::new();
         match renderer.render_report(&mut out, self) {
-            Ok(()) => out,
-            Err(render_error) => format!("{} (could not render: {})", self.syn, render_error),
+            Ok(()) => {
+                if out.lines().count() > 50 {
+                    out = out.lines().take(49).collect::<Vec<_>>().join("\n");
+                    out.push_str("\n<truncated>")
+                }
+                out
+            }
+            Err(render_error) => format!(
+                "{} at offset {} (could not render: {})",
+                self.inner,
+                self.span.offset(),
+                render_error
+            ),
         }
     }
 }
