50cd965dcb9dffa7071d6a70f5a6a8f6b465ec77
Aatif Syed <aatifsyedyp@gmail.com>
2024-01-20 17:22:35 +0000

    feat: generic petgraph support

--- src/pet.rs
+++ src/pet.rs
@@ -4,13 +4,16 @@ use std::{
         hash_map::Entry::{Occupied, Vacant},
         HashMap,
     },
+    iter,
 };
-
-use petgraph::{graphmap::GraphMap, EdgeType};
 
 use crate::dot::{
     self, Attrs, EdgeDirectedness, EdgeTarget, Graph, GraphDirectedness, NodeId, Stmt, StmtEdge,
     StmtList, StmtNode, ID,
+};
+use petgraph::{
+    data::Build,
+    visit::{GetAdjacencyMatrix, GraphProp},
 };
 
 macro_rules! bail {
@@ -25,28 +28,41 @@ pub struct PortlessNode<'a> {
     pub attrs: Option<&'a Attrs>,
 }
 
-pub fn from_dot<Di: EdgeType>(
-    dot_graph: &dot::Graph,
-) -> syn::Result<GraphMap<PortlessNode<'_>, Option<&Attrs>, Di>> {
+#[derive(Clone, Copy, Debug, Hash, PartialEq, Eq, PartialOrd, Ord)]
+struct NodeInfo<'a, Ix> {
+    portless_node: PortlessNode<'a>,
+    node_id: Ix,
+}
+
+pub fn from_dot<'a, G>(dot_graph: &'a dot::Graph) -> syn::Result<G>
+where
+    G: Default
+        + Build<NodeWeight = PortlessNode<'a>, EdgeWeight = Option<&'a Attrs>>
+        + GraphProp
+        + GetAdjacencyMatrix,
+{
     let Graph {
-        strict: _,
+        strict,
         directedness,
         id: _,
         brace_token: _,
         stmt_list: StmtList { stmts },
     } = dot_graph;
-    match (directedness, Di::is_directed()) {
+
+    let mut petgraph = G::default();
+
+    match (directedness, petgraph.is_directed()) {
         (GraphDirectedness::Digraph(_), true) | (GraphDirectedness::Graph(_), false) => {}
         (GraphDirectedness::Graph(_), true) | (GraphDirectedness::Digraph(_), false) => bail!(
             directedness,
-            "graph direction in dot does not match compile-time direction {}",
-            type_name::<Di>()
+            "graph direction in dot does not match compile-time direction of `{}`",
+            type_name::<G>()
         ),
     }
 
-    let mut id2portless_node = HashMap::new();
+    let mut id2info = HashMap::new();
 
-    for (stmt, _semi) in stmts {
+    for (stmt, _) in stmts {
         if let Stmt::Node(StmtNode {
             node_id: NodeId { id, port },
             attrs,
@@ -55,19 +71,53 @@ pub fn from_dot<Di: EdgeType>(
             if port.is_some() {
                 bail!(port, "nodes with ports are not supported")
             };
-            match id2portless_node.entry(id) {
-                Occupied(_) => bail!(id, "concatenation of node attributes is not supported"),
+            match id2info.entry(id) {
+                Occupied(_) => bail!(id, "duplicate node statements are not supported"),
                 Vacant(it) => {
-                    it.insert(PortlessNode {
+                    let portless_node = PortlessNode {
                         id,
                         attrs: attrs.as_ref(),
+                    };
+                    let node_id = petgraph.add_node(portless_node);
+                    it.insert(NodeInfo {
+                        portless_node,
+                        node_id,
                     });
                 }
             }
         }
     }
 
-    let mut petgraph = GraphMap::<_, _, Di>::default();
+    for (stmt, _) in stmts {
+        if let Stmt::Edge(StmtEdge {
+            from,
+            edges,
+            attrs: _,
+        }) = stmt
+        {
+            for it in iter::once(from).chain(edges.iter().map(|(_, it)| it)) {
+                match it {
+                    EdgeTarget::Subgraph(it) => bail!(it, "subgraphs are not supported"),
+                    EdgeTarget::NodeId(NodeId { id, port }) => {
+                        if port.is_some() {
+                            bail!(port, "nodes with ports are not supported");
+                        }
+                        match id2info.entry(id) {
+                            Occupied(_) => {}
+                            Vacant(it) => {
+                                let portless_node = PortlessNode { id, attrs: None };
+                                let node_id = petgraph.add_node(portless_node);
+                                it.insert(NodeInfo {
+                                    portless_node,
+                                    node_id,
+                                });
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 
     for (stmt, _semi) in stmts {
         match stmt {
@@ -78,7 +128,7 @@ pub fn from_dot<Di: EdgeType>(
             Stmt::Edge(StmtEdge { from, edges, attrs }) => {
                 let mut target_from = from;
                 for (directedness, target_to) in edges {
-                    match (directedness, Di::is_directed()) {
+                    match (directedness, petgraph.is_directed()) {
                         (EdgeDirectedness::Directed(_), true)
                         | (EdgeDirectedness::Undirected(_), false) => {}
                         (EdgeDirectedness::Directed(_), false)
@@ -87,33 +137,31 @@ pub fn from_dot<Di: EdgeType>(
                         }
                     }
 
-                    let portless_node_from = match target_from {
-                        EdgeTarget::Subgraph(it) => bail!(it, "unsupported edge target"),
-                        EdgeTarget::NodeId(NodeId { id, port }) => match port {
-                            Some(port) => bail!(port, "nodes with ports are not supported"),
-                            None => id2portless_node
-                                .get(id)
-                                .copied()
-                                .unwrap_or(PortlessNode { id, attrs: None }),
-                        },
+                    let EdgeTarget::NodeId(NodeId { id, port: None }) = target_from else {
+                        unreachable!();
                     };
-                    let portless_node_to = match target_to {
-                        EdgeTarget::Subgraph(it) => bail!(it, "unsupported edge target"),
-                        EdgeTarget::NodeId(NodeId { id, port }) => match port {
-                            Some(port) => bail!(port, "nodes with ports are not supported"),
-                            None => id2portless_node
-                                .get(id)
-                                .copied()
-                                .unwrap_or(PortlessNode { id, attrs: None }),
-                        },
+                    let node_id_from = id2info[id].node_id;
+                    let EdgeTarget::NodeId(NodeId { id, port: None }) = target_to else {
+                        unreachable!();
                     };
+                    let node_id_to = id2info[id].node_id;
 
-                    let clobbered =
-                        petgraph.add_edge(portless_node_from, portless_node_to, attrs.as_ref());
-                    if clobbered.is_some() {
+                    if strict.is_some()
+                        && petgraph.is_adjacent(
+                            &petgraph.adjacency_matrix(),
+                            node_id_from,
+                            node_id_to,
+                        )
+                    {
+                        bail!(stmt, "duplicate edge on a strict graph")
+                    }
+
+                    let edge_id = petgraph.add_edge(node_id_from, node_id_to, attrs.as_ref());
+                    if edge_id.is_none() {
                         bail!(
                             stmt,
-                            "duplicate edges are not supported (graphs are implicitly `strict`)"
+                            "could not insert parallel edge: operation not supported by {}",
+                            type_name::<G>()
                         )
                     }
 
@@ -128,10 +176,11 @@ pub fn from_dot<Di: EdgeType>(
 
 #[cfg(test)]
 mod tests {
-    use petgraph::{Directed, Undirected};
+    use petgraph::{graphmap::GraphMap, Directed, Undirected};
     use syn::parse_quote;
 
     use super::*;
+
     #[test]
     fn test_dot() {
         let dot_graph: dot::Graph = parse_quote! {
@@ -139,16 +188,16 @@ mod tests {
                 a -- b -- c
             }
         };
-        let petgraph = from_dot::<Undirected>(&dot_graph).unwrap();
+        let petgraph = from_dot::<GraphMap<_, _, Undirected>>(&dot_graph).unwrap();
         assert_eq!(petgraph.node_count(), 3);
         assert_eq!(petgraph.edge_count(), 2);
 
         let dot_graph: dot::Graph = parse_quote! {
             digraph {
-                a -> b -> c
+                a -> b -> c;
             }
         };
-        let petgraph = from_dot::<Directed>(&dot_graph).unwrap();
+        let petgraph = from_dot::<GraphMap<_, _, Directed>>(&dot_graph).unwrap();
         assert_eq!(petgraph.node_count(), 3);
         assert_eq!(petgraph.edge_count(), 2);
     }
